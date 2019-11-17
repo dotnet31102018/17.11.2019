8823a4d2a6caaffe725afb09b8776a64d2a5c5ea
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-11-17 18:30:23 +0200

    Add files via upload

---
+++ MoviesAjax/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movies1711.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Rank { get; set; }
+        public string Genre { get; set; }
+        public Movie()
+        {
+
+        }
+
+        public Movie(int id, string name, int year, int rank, string genre)
+        {
+            Id = id;
+            Name = name;
+            Year = year;
+            Rank = rank;
+            Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Year} {Rank} {Genre}";
+        }
+    }
+}
